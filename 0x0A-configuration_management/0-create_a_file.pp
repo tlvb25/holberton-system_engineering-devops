8fc1ac20432586fc18ec44a60171a52d604beb66
T <test@mail.com>
2019-08-05 22:50:42 -0700

    files for task 1 - 3

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,1 +1,8 @@
-#!/usr/bin/env bash
+# puppet manifest creates a file
+file { '/tmp/holberton':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   =>  'www-data',
+  content => 'I love Puppet',
+}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,1 +1,5 @@
-#!/usr/bin/env bash
+# create a manifest that kills a process named killmenow
+exec { 'killmenow':
+  command   => 'pkill killmenow',
+  provider  => 'shell',
+}
