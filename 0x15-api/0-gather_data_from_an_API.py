c0add01ac7c27e392f457ddfab606e66e29946d8
T <test@mail.com>
2019-09-16 17:58:21 -0700

    code update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,6 +3,24 @@ from sys import argv
 import requests
 
 if __name__ == "__main__":
-    r = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                     .format(sys.argv[1]))
-print (r)
+    response = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                     .format(argv[1]))
+
+    json_obj = response.json()
+
+    emp_name = json_obj.get('name')
+
+    tasks, completed = 0, 0
+
+    tasks_items = []
+
+    for task in json_obj:
+        tasks += 1
+        if task.get('completed'):
+                completed += 1
+                tasks_items.append(task.get('title'))
+    print ('Employee {} is done with tasks({}/{}}):'
+           .format(emp_name, completed, tasks))
+
+    for t in tasks_items:
+        print('\t', t)
