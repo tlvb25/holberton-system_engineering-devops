b1dbf7ce1a1364439bfd66913576eb802a3f9422
T <test@mail.com>
2019-09-16 17:13:10 -0700

    testing code

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env bash
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    output = fetch('https://jsonplaceholder.typicode.com/users/argv[1]/todos')
+    .then(response => response.json())
+    .then(json => console.log(json))
