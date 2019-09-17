e647bbc8adb271a5ca346578b92cf3a0852dc0f1
T <test@mail.com>
2019-09-16 21:45:37 -0700

    code corr

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -19,13 +19,13 @@ if __name__ == "__main__":
     for n in name_obj:
         usrid = n.get('id')
         emp_name = n.get('username')
-        tasks = []
-        for task in tasks_obj:
-            if task.get('userId') == usrid:
-                d = {"username": emp_name, "task": task.get('title'),
-                     "completed": task.get('completed')}
-                tasks.append(d)
-        json_f[usrid] = tasks
+        tasks_items = []
+        for task_items in tasks_obj:
+            if task_items.get('userId') == usrid:
+                d = {"username": emp_name, "task": task_items.get('title'),
+                     "completed": task_items.get('completed')}
+                tasks_items.append(d)
+        json_f[usrid] = tasks_items
 
-    with open("todo_all_employees.json", mode='w') as file:
-        json.dump(jsonDict, file)
+    with open("todo_all_employees.json", mode='w') as json_file:
+        json.dump(jsonDict, json_file)
