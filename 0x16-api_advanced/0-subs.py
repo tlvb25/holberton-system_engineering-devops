77f1d0a9ee175570bf113f7ea3def4b1aa852282
T <test@mail.com>
2019-09-19 23:15:45 -0700

    script for task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env bash
+import json
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """0. How many subs? """
+    base_url = 'https://www.reddit.com/r/{}/.json'.format(subreddit)
+
+    head = {'User-Agent': 'Reddit API test'}
+
+    response = requests.get(base_url, headers=head, allow_redirects=False)
+
+    json_dict = response.json()
+
+    if json_dict.get('error', 200) == 404:
+        return 0
+    return json_dict.get('data').get('children')[0]\
+                         .get('data').get('subreddit_subscribers')
