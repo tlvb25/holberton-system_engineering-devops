865c03191fc4d535cff9fba2bb447669a6b97f1d
T <test@mail.com>
2019-09-19 23:35:58 -0700

    script for task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,25 @@
-#!/usr/bin/env bash
+#!/usr/bin/python3
+"""1. Top Ten"""
+import requests
+
+
+def top_ten(subreddit):
+    """queries the Reddit API"""
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+
+    header = {'User-Agent': 'Reddit API test'}
+
+    params = {'limit': 10}
+
+    response = requests.get(url, headers=header, allow_redirects=False, params=params)
+
+    json_dict = response.json()
+
+    if json_dict.get('error', 200) == 404:
+        print(None)
+        return
+
+    results = json_dict.get('data').get('children')
+    
+    for dic in results[:10]:
+        print(dic.get('data').get('title'))
